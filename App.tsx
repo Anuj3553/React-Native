55ceb772ea6c51d9d8b4a49db4fa6ef13f0ab3a5
Anuj3553 <anujverma3553@gmail.com>
2024-12-31 23:51:45 +0530

    React Native Flexbox

--- App.tsx
+++ App.tsx
@@ -1,49 +1,15 @@
+import { SafeAreaView, ScrollView } from 'react-native';
 import React from 'react';
+import FlatCards from './components/FlatCards';
 
-import {
-  View,
-  Text,
-  SafeAreaView,
-  Button,
-  Alert,
-  useColorScheme,
-  StyleSheet,
-} from 'react-native';
-
-function App(): JSX.Element {
-
-  const isDarkMode = useColorScheme() === 'dark'; // true if dark mode is enabled
-
-  function alert(message: string) {
-    Alert.alert('Alert', message);
-  }
-
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={isDarkMode ? styles.lightText : styles.darkText}>Hello World 🌍</Text>
-        <Text style={isDarkMode ? styles.lightText : styles.darkText}>Hello World 🌍</Text>
-        <Text style={isDarkMode ? styles.lightText : styles.darkText}>Hello World 🌍</Text>
-        <Text style={isDarkMode ? styles.lightText : styles.darkText}>Hello World 🌍</Text>
-        <Text style={isDarkMode ? styles.lightText : styles.darkText}>Hello World 🌍</Text>
-        <Button title="Click me" onPress={() => alert('Button clicked')} />
-      </View>
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+      </ScrollView>
     </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  lightText: {
-    color: 'white',
-  },
-  darkText: {
-    color: 'black',
-  },
-});
+};
 
 export default App;
