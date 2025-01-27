66d401408d140cb43762a22f268c89612d9130e6
Anuj3553 <anujverma3553@gmail.com>
2025-01-27 21:29:42 +0530

    Add react-native-snackbar for notifications and implement game logic in App component, Finishing tic tac toe in react native

--- project6/src/components/Icons.tsx
+++ project6/src/components/Icons.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { PropsWithChildren } from 'react';
+import type { PropsWithChildren } from 'react';
 
-import Icons from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type IconsProps = PropsWithChildren<{
     name: string;
-}>;
+}>
 
 const Icons = ({ name }: IconsProps) => {
     switch (name) {
         case 'circle':
-            return <Icons name="circle-thin" size={38} color="#F7CD2E" />;
-            break;
+            return <Icon name="circle-thin" size={38} color="#F7CD2E" />;
         case 'cross':
-            return <Icons name="times" size={38} color="#38CC77" />;
-            break;
+            return <Icon name="times" size={38} color="#38CC77" />;
         default:
-            return <Icons name="pencil" size={38} color="#0D0D0D" />;
+            return <Icon name="pencil" size={38} color="#0D0D0D" />;
     }
 };
 
+export default Icons;
